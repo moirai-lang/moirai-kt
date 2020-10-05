e2aa8ea5b41843c42d1ef898dd719a6f3853f613
Bryan Croteau <bdcroteau@gmail.com>
2020-10-05 13:49:57 -0700

    Add support for building and locally publishing a superjar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+plugins {
+    java
+    `maven-publish`
+}
+
 group = "com.tsikhe"
 version = "0.1"
 
+repositories {
+    mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("default") {
+            from(components["java"])
+        }
+    }
+}
