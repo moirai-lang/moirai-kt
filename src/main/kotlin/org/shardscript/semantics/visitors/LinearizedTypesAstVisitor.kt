45161c272785d0abae2445f3cf1ccd57c34a44a1
sithys <bdcroteau@gmail.com>
2023-09-17 15:43:53 -0700

    Linearize types

--- src/main/kotlin/org/shardscript/semantics/visitors/LinearizedTypesAstVisitor.kt
+++ src/main/kotlin/org/shardscript/semantics/visitors/LinearizedTypesAstVisitor.kt
@@ -35,7 +35,7 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
                     val res: MutableSet<LinearizedSymbol> = HashSet()
                     val paramChildren: MutableList<TypeParamChildren> = ArrayList()
                     parameterizedSymbol.typeParams.zip(symbol.substitutionChain.replayArgs()).forEach {
-                        val children = linearize(ctx, it.second)
+                        val children = linearize(ctx, it.second as Symbol)
                         paramChildren.add(TypeParamChildren(it.first, children))
                     }
                     res.add(TypeArgChildren(ctx, symbol, paramChildren))
@@ -45,7 +45,7 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
                     val res: MutableSet<LinearizedSymbol> = HashSet()
                     val paramChildren: MutableList<TypeParamChildren> = ArrayList()
                     parameterizedSymbol.typeParams.zip(symbol.substitutionChain.replayArgs()).forEach {
-                        val children = linearize(ctx, it.second)
+                        val children = linearize(ctx, it.second as Symbol)
                         paramChildren.add(TypeParamChildren(it.first, children))
                     }
                     res.add(TypeArgChildren(ctx, symbol, paramChildren))
@@ -55,13 +55,13 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
                     val res: MutableSet<LinearizedSymbol> = HashSet()
                     parameterizedSymbol.formalParams.forEach {
                         val ofTypeSymbol = symbol.substitutionChain.replay(it.ofTypeSymbol)
-                        res.addAll(linearize(ctx, ofTypeSymbol))
+                        res.addAll(linearize(ctx, ofTypeSymbol as Symbol))
                     }
                     val returnType = symbol.substitutionChain.replay(parameterizedSymbol.returnType)
-                    res.addAll(linearize(ctx, returnType))
+                    res.addAll(linearize(ctx, returnType as Symbol))
                     val paramChildren: MutableList<TypeParamChildren> = ArrayList()
                     parameterizedSymbol.typeParams.zip(symbol.substitutionChain.replayArgs()).forEach {
-                        val children = linearize(ctx, it.second)
+                        val children = linearize(ctx, it.second as Symbol)
                         paramChildren.add(TypeParamChildren(it.first, children))
                     }
                     res.add(TypeArgChildren(ctx, symbol, paramChildren))
@@ -71,13 +71,13 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
                     val res: MutableSet<LinearizedSymbol> = HashSet()
                     parameterizedSymbol.formalParams.forEach {
                         val ofTypeSymbol = symbol.substitutionChain.replay(it.ofTypeSymbol)
-                        res.addAll(linearize(ctx, ofTypeSymbol))
+                        res.addAll(linearize(ctx, ofTypeSymbol as Symbol))
                     }
                     val returnType = symbol.substitutionChain.replay(parameterizedSymbol.returnType)
-                    res.addAll(linearize(ctx, returnType))
+                    res.addAll(linearize(ctx, returnType as Symbol))
                     val paramChildren: MutableList<TypeParamChildren> = ArrayList()
                     parameterizedSymbol.typeParams.zip(symbol.substitutionChain.replayArgs()).forEach {
-                        val children = linearize(ctx, it.second)
+                        val children = linearize(ctx, it.second as Symbol)
                         paramChildren.add(TypeParamChildren(it.first, children))
                     }
                     res.add(TypeArgChildren(ctx, symbol, paramChildren))
@@ -87,13 +87,13 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
                     val res: MutableSet<LinearizedSymbol> = HashSet()
                     parameterizedSymbol.formalParams.forEach {
                         val ofTypeSymbol = symbol.substitutionChain.replay(it.ofTypeSymbol)
-                        res.addAll(linearize(ctx, ofTypeSymbol))
+                        res.addAll(linearize(ctx, ofTypeSymbol as Symbol))
                     }
                     val returnType = symbol.substitutionChain.replay(parameterizedSymbol.returnType)
-                    res.addAll(linearize(ctx, returnType))
+                    res.addAll(linearize(ctx, returnType as Symbol))
                     val paramChildren: MutableList<TypeParamChildren> = ArrayList()
                     parameterizedSymbol.typeParams.zip(symbol.substitutionChain.replayArgs()).forEach {
-                        val children = linearize(ctx, it.second)
+                        val children = linearize(ctx, it.second as Symbol)
                         paramChildren.add(TypeParamChildren(it.first, children))
                     }
                     res.add(TypeArgChildren(ctx, symbol, paramChildren))
@@ -113,18 +113,18 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
             val res: MutableSet<LinearizedSymbol> = HashSet()
             res.add(SymbolOnly(ctx, symbol))
             symbol.formalParams.forEach {
-                res.addAll(linearize(ctx, it.ofTypeSymbol))
+                res.addAll(linearize(ctx, it.ofTypeSymbol as Symbol))
             }
-            res.addAll(linearize(ctx, symbol.returnType))
+            res.addAll(linearize(ctx, symbol.returnType as Symbol))
             res
         }
         is LambdaSymbol -> {
             val res: MutableSet<LinearizedSymbol> = HashSet()
             res.add(SymbolOnly(ctx, symbol))
             symbol.formalParams.forEach {
-                res.addAll(linearize(ctx, it.ofTypeSymbol))
+                res.addAll(linearize(ctx, it.ofTypeSymbol as Symbol))
             }
-            res.addAll(linearize(ctx, symbol.returnType))
+            res.addAll(linearize(ctx, symbol.returnType as Symbol))
             res
         }
         is GroundMemberPluginSymbol -> setOf(SymbolOnly(ctx, symbol))
@@ -132,9 +132,9 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
             val res: MutableSet<LinearizedSymbol> = HashSet()
             res.add(SymbolOnly(ctx, symbol))
             symbol.formalParamTypes.forEach {
-                res.addAll(linearize(ctx, it))
+                res.addAll(linearize(ctx, it as Symbol))
             }
-            res.addAll(linearize(ctx, symbol.returnType))
+            res.addAll(linearize(ctx, symbol.returnType as Symbol))
             res
         }
         is FinTypeSymbol -> setOf(SymbolOnly(ctx, symbol))
@@ -144,8 +144,8 @@ fun linearize(ctx: SourceContext, symbol: Symbol): Set<LinearizedSymbol> =
         is SumCostExpression -> setOf(SymbolOnly(ctx, symbol))
         is ProductCostExpression -> setOf(SymbolOnly(ctx, symbol))
         is MaxCostExpression -> setOf(SymbolOnly(ctx, symbol))
-        is FunctionFormalParameterSymbol -> linearize(ctx, symbol.ofTypeSymbol)
-        is FieldSymbol -> linearize(ctx, symbol.ofTypeSymbol)
+        is FunctionFormalParameterSymbol -> linearize(ctx, symbol.ofTypeSymbol as Symbol)
+        is FieldSymbol -> linearize(ctx, symbol.ofTypeSymbol as Symbol)
         is PlatformFieldSymbol -> setOf(SymbolOnly(ctx, symbol))
         is LocalVariableSymbol -> setOf(SymbolOnly(ctx, symbol))
         is ParameterizedFunctionSymbol -> setOf(SymbolOnly(ctx, symbol))
@@ -159,57 +159,57 @@ class LinearizedTypesAstVisitor : UnitAstVisitor() {
 
     override fun visit(ast: IntLiteralAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: DecimalLiteralAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: BooleanLiteralAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: CharLiteralAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: StringLiteralAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: StringInterpolationAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: LetAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.ofTypeSymbol))
+        linearized.addAll(linearize(ast.ctx, ast.ofTypeSymbol as Symbol))
     }
 
     override fun visit(ast: RefAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: FileAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: BlockAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: FunctionAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
         ast.formalParams.forEach {
             linearized.addAll(linearize(ast.ctx, ast.scope.fetch(it.ofType)))
         }
@@ -218,7 +218,7 @@ class LinearizedTypesAstVisitor : UnitAstVisitor() {
 
     override fun visit(ast: RecordDefinitionAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
         ast.fields.forEach {
             linearized.addAll(linearize(ast.ctx, ast.scope.fetch(it.ofType)))
         }
@@ -226,51 +226,51 @@ class LinearizedTypesAstVisitor : UnitAstVisitor() {
 
     override fun visit(ast: ObjectDefinitionAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: DotAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: GroundApplyAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: DotApplyAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: ForEachAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: AssignAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: DotAssignAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: IfAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: AsAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.readType()))
+        linearized.addAll(linearize(ast.ctx, ast.readType() as Symbol))
     }
 
     override fun visit(ast: IsAst) {
         super.visit(ast)
-        linearized.addAll(linearize(ast.ctx, ast.identifierSymbol))
+        linearized.addAll(linearize(ast.ctx, ast.identifierSymbol as Symbol))
     }
 }
