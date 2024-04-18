8fb0596f15d135671a792001007f8903a011f880
Bryan Croteau <bdcroteau@gmail.com>
2024-04-17 17:00:00 -0700

    Fix bug with transient scripts

--- src/main/kotlin/moirai/composition/ImportsParseTreeListener.kt
+++ src/main/kotlin/moirai/composition/ImportsParseTreeListener.kt
@@ -15,8 +15,15 @@ internal class ImportsParseTreeListener(val errors: LanguageErrors) :
     private var scriptType: ScriptType = PureTransient
 
     override fun enterTransientScript(ctx: MoiraiParser.TransientScriptContext) {
-        val nameParts = ctx.importIdSeq().IDENTIFIER().map { it.symbol.text }
-        scriptType = TransientScript(nameParts)
+        val import = ctx.importIdSeq().IDENTIFIER().map { it.symbol.text }
+        scriptType = TransientScript(import)
+        val sourceContext = createContext(scriptType.fileName(), ctx.start)
+
+        if (accumulatedImports.contains(import)) {
+            errors.add(sourceContext, DuplicateImport(import))
+        } else {
+            accumulatedImports[import] = sourceContext
+        }
     }
 
     override fun enterScriptStat(ctx: MoiraiParser.ScriptStatContext) {
