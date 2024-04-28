c1d66f16803d6a6a1907afba041eb3cd4d5704c7
Bryan Croteau <bdcroteau@gmail.com>
2024-04-28 16:25:05 -0700

    Add char to transport Ast

--- src/main/kotlin/moirai/transport/TransportAst.kt
+++ src/main/kotlin/moirai/transport/TransportAst.kt
@@ -14,6 +14,7 @@ sealed class TransportAst
 data class IntLiteralTransportAst(val canonicalForm: Int): TransportAst()
 data class DecimalLiteralTransportAst(val canonicalForm: BigDecimal): TransportAst()
 data class BooleanLiteralTransportAst(val canonicalForm: Boolean): TransportAst()
+data class CharLiteralTransportAst(val canonicalForm: Char): TransportAst()
 data class StringLiteralTransportAst(val canonicalForm: String): TransportAst()
 data class RefTransportAst(val name: String): TransportAst()
 data class ApplyTransportAst(val name: String, val args: List<TransportAst>): TransportAst()
